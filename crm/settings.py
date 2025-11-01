88649febb404921f5eee48fdf3b4abae68448542
KEVIN NSHUTI <nkevin881@gmail.com>
2025-11-01 14:40:24 +0200

    creating a settings file in crm for alx checker

--- crm/settings.py
+++ crm/settings.py
@@ -2,3 +2,15 @@ CRONJOBS = [
     ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
 ]
 
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "graphene_django",
+    "crm",
+    "django_filters",
+    "django_crontab",
+]
